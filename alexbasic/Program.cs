ac37367eef0b6ec8e8a726b09129e4aec2e9a725
richie <richie.kwon1@gmail.com>
2022-01-26 08:01:06 +0000

    enum2

---
+++ alexbasic/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+
+namespace alexbasic
+{
+    class Program
+    {
+        enum Yera
+        {
+            Fishing,
+            Cooking,
+            Boarding
+        }
+
+        [Flags]
+        enum Boarder
+        {
+            None =0,
+            Top =1,
+            Right=2,
+            Bottom=4,
+            Left=8
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string name = "richie";
+            Console.WriteLine(name);
+
+            // int[] a = new int[100];
+            // for (int i=0 ; i<100; i++)
+            // {
+            //     a[i] = i + 1;
+            // }
+
+            // int[] b = new int[] { };
+            // b[0] = 7;
+            // for (int i = 0; i <100; i++)
+            // {
+            //     b[i] = i ;
+            // }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 100; i++)
+            {
+                sb.Append(i.ToString());
+            }
+
+            Boarder b = Boarder.Bottom;
+        }
+    }
+}
---
+++ bluepopes/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using bluepopes.Models;
+
+namespace bluepopes.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public IActionResult Test(List<int> x, string y)
+        {
+            return View();
+        }
+        public IActionResult Test2(string x, string y)
+        {
+            ViewData["x"] = x;
+            ViewBag.y = y;
+            return PartialView();
+        }
+        
+        public IActionResult Test3(List<int> x, string y)
+        {
+            var model = new TestModel();
+            model.X = 1;
+            model.Y = "Yera lee";
+            
+            return Json(model);
+        }
+        
+        public IActionResult Test4(string x, string y)
+        {
+            ViewData["x"] = x;
+            ViewBag.y = y;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
