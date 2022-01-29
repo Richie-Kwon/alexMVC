bcdae15edd84afb977ee71e7801729182bab84db
richie <richie.kwon1@gmail.com>
2022-01-29 16:18:31 +0000

    alex3

---
+++ alexbasic2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace alexbasic2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int cel = 12;
+            double fire = (cel * 9 / 5) + 32;
+            string option = args[0];
+            Console.WriteLine(option);
+
+            // mcs : C# compiler, mono - Mono runtime
+            List<int> numberList = new List<int>();
+            numberList.Add(32);
+            numberList.Add(91);
+            foreach (var s in numberList)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+} 
